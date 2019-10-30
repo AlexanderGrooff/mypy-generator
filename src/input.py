f6b1339ec61e47be843059401e5445dc1ae7f949
Alexander Grooff <alexandergrooff@gmail.com>
2019-10-30 22:53:01 +0100

    Add several helper functions

--- src/input.py
+++ src/input.py
@@ -2,7 +2,8 @@ import argparse
 
 
 def parse_args(args=None):
-    parser = argparse.ArgumentParser()
-    parser.add_argument("path", help="Path to Python file or directory containing Python files.")
+    parser = argparse.ArgumentParser(description="Generate type annotations for Python files in the given path")
+    parser.add_argument("path", help="Path to Python file or directory containing Python files")
     parser.add_argument("-i", "--in-place", help="Whether or not to make changes in place", action='store_true')
+
     return parser.parse_args(args=args)
--- src/parser.py
+++ src/parser.py
@@ -1,6 +1,11 @@
+import importlib
+import inspect
 import os
 from functools import reduce
-from typing import List
+from typing import List, Callable
+
+
+module_type = type(os)
 
 
 def is_python_file(path: str) -> bool:
@@ -13,18 +18,53 @@ def find_python_files(path: str) -> List[str]:
     :param str path: Path to file or directory.
     :rtype: [str, ..]
     """
+    path = os.path.relpath(os.path.abspath(path), os.getcwd())
     if not os.path.exists(path):
         raise RuntimeError("Path {} does not exist".format(path))
     if os.path.isfile(path):
         return [path] if is_python_file(path) else []
     if os.path.isdir(path):
-        return reduce(lambda files, child_path: files + find_python_files(child_path), os.listdir(path), [])
+        return reduce(lambda files, child_path: files + find_python_files("{}/{}".format(path, child_path)),
+                      os.listdir(path), [])
 
 
-def get_functions_from_file(path: str):
+def file_to_module(path: str) -> module_type:
     """
-    Find all functions defined in the given Python file.
-    :param path:
+    Import a module from a given path if possible
+    :param path: Path to a Python file
     :return:
     """
+    return importlib.import_module(path.replace('/', '.').replace('.py', ''))
+
+
+def is_function(obj, allow_builtin: bool=False) -> bool:
+    """
+    Check if the given object is a function
+    :param obj: Possible function object
+    :param allow_builtin: Whether or not to allow builtin functions
+    :return: True if the object is a function
+    """
+    return inspect.isfunction(obj) or (allow_builtin and inspect.isbuiltin(obj))
+
+
+def functions_in_module(module: module_type, include_builtins: bool=False) -> List[Callable]:
+    """
+    Retrieve all functions from a given module. Exclude builtin functions if specified
+    :param module: Module to look for functions
+    :param include_builtins: Whether or not to include builtin functions such as __hash__
+    :return: List of functions in module
+    """
+    members = inspect.getmembers(module)
+    return [obj for _, obj in members if is_function(obj, allow_builtin=include_builtins)]
+
+
+def used_functions_in_function(f: Callable, include_unbound_members: bool=False) -> List[Callable]:
+    """
+    Find all functions that are being used in the given function.
+    :param f: Function to check
+    :param include_unbound_members: Whether or not to include unbound members in the result
+    :return: List of functions that are called
+    """
+    closure_vars = inspect.getclosurevars(f)
+    return [obj for obj in closure_vars.globals.values() if is_function(obj)]
 
